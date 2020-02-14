ef4fd7c768e3e9f1ab8c871f0de384afd7e3cf41
Dare Adewole <dare.dev.adewole@gmail.com>
2020-02-14 23:02:21 +0100

    create challenges API

--- src/api/challenge.api.js
+++ src/api/challenge.api.js
@@ -1,6 +1,54 @@
+import $axios from '/api/api'
 
-export default class ChallengApi {
+export default class ChallengeApi {
+
+    /**
+     * 
+     * @param {*} payload
+     * 
+     *      Create a Challenge 
+     */
     static createChallenge (payload) {
-        
+        return $axios.post('/savers', payload)
+    }
+
+    /**
+     * 
+     * @param {*} planCode 
+     * @param {*} payload 
+     * 
+     *      Join a Challenge
+     */
+    static joinChallenge (planCode, { request }) {
+        return $axios.post()
+    }
+
+    /**
+     *      Get Challenges in the public domain
+     */
+    static getPublicChallenges () {
+        return $axios.get('/savers/public')
+    }
+
+    /**
+     * Get Personally created challenges
+     */
+    static getMyChallenges () {
+        return $axios.get('/savers/my-plans')
+    }
+
+    static updateChallenge (saver, payload) {
+        return $axios.put(`/savers/${saver}`, payload)
+    }
+
+    /**
+     * 
+     * @param {*} saver 
+     * @param {*} param1 
+     * 
+     *      Leave a challenge
+     */
+    static leaveChallenge (saver, { Saver }) {
+        return $axios.delete(`savers/${saver}`, { Saver })
     }
 }
